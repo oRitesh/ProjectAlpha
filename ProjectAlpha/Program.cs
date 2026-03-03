595f2b2d2d69ff6cf51ee867272e801a9a4f0a7d
MGnaoui <1118084@hr.nl>
2026-03-03 13:36:11 +0100

    Moved all classes in Program.cs

--- ProjectAlpha/Program.cs
+++ ProjectAlpha/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public static class Weapon
+{
+
+    public int Id;
+    public int MaximumDamage;
+    public string Name;
+
+    public Weapon(int id, int maximumDamage, string name)
+    {
+        this.Id = id;
+        this.MaximumDamage = maximumDamage;
+        this.Name = name;
+    }
+
+    public void Attack()
+    {
+        
+    }
+
+}
+public class SuperAdventure
+{
+    public string CurrentMonster;
+    public string ThePlayer;
+    public SuperAdventure(string currentMonster, string thePlayer)
+    {
+        this.CurrentMonster = currentMonster;
+        this.ThePlayer = thePlayer;
+    }
+}
+public class Quest
+{
+    public string Description;
+    public int Id;
+    public string Name;
+    public Quest(string description, int id, string name)
+    {
+        this.Description = description;
+        this.Id = id;
+        this.Name = name;
+    }
+}
+public class Player
+{
+    public int CurrentHitPoints;
+    public string CurrentLocation;
+    public string CurrentWeapon;
+    public int MaximumHitPoints;
+    public string Name;
+    public Player(int currentHitPoints, string currentLocation, string currentWeapon, int maximumHitPoints, string Name)
+    {
+        this.CurrentHitPoints = currentHitPoints;
+        this.CurrentLocation = currentLocation;
+        this.CurrentWeapon = currentWeapon;
+        this.MaximumHitPoints = maximumHitPoints;
+        this.Name = name;
+    }
+}
+public class Monster
+{
+    public string Description;
+    public int Id;
+    public string Name;
+    public Monster(string description, int id, string name)
+    {
+        this.Description = description;
+        this.Id = id;
+        this.Name = name;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
