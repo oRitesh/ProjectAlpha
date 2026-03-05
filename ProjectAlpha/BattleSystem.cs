4120001c684db214d351bf40bdd8480e4b0f6e42
JakinHR <1128775@hr.nl>
2026-03-05 18:37:46 +0100

    errors fixen en runversie veranderd naar net10.0

--- ProjectAlpha/BattleSystem.cs
+++ ProjectAlpha/BattleSystem.cs
@@ -8,39 +8,46 @@ public static class BattleSystem
 
         while (player.CurrentHitPoints > 0 && monster.CurrentHitPoints > 0)
         {
-            Console.WriteLine("1 - Attack");
-            Console.WriteLine("2 - Flee");
-            string choice = Console.ReadLine();
+            Console.WriteLine("A - Attack");
+            Console.WriteLine("F - Flee");
+            string choice = Console.ReadLine().ToUpper();
 
-            if (choice == "2")
+            if (choice == "F")
             {
-                Console.WriteLine("You fled from the battle.");
                 return false;
             }
+
             bool isPlayerHit = new Random().NextDouble() > PlayerHitChance;
             if (isPlayerHit)
             {
                 Console.WriteLine("Your attack missed!");
                 Console.WriteLine("The " + monster.Name + " has " + monster.CurrentHitPoints + " HP left.");
+                Console.WriteLine();
             }
             else
             {
                 monster.TakeDamage(player.CurrentWeapon.MaximumDamage);
                 Console.WriteLine("You hit the " + monster.Name + " for " + player.CurrentWeapon.MaximumDamage + " damage.");
                 Console.WriteLine("The " + monster.Name + " has " + monster.CurrentHitPoints + " HP left.");
+                Console.WriteLine();
             }
 
-            bool isMonsterHit = new Random().NextDouble() >= MonsterHitChance;
-            if (isMonsterHit)
+            if (monster.CurrentHitPoints > 0)
             {
-                Console.WriteLine("The " + monster.Name + "'s attack missed!");
-                Console.WriteLine("You have " + player.CurrentHitPoints + " HP left.");
-            }
-            else
-            {
-                player.TakeDamage(monster.MaximumDamage);
-                Console.WriteLine("The " + monster.Name + " hit you for " + monster.MaximumDamage + " damage.");
-                Console.WriteLine("You have " + player.CurrentHitPoints + " HP left.");
+                bool isMonsterHit = new Random().NextDouble() >= MonsterHitChance;
+                if (isMonsterHit)
+                {
+                    Console.WriteLine("The " + monster.Name + "'s attack missed!");
+                    Console.WriteLine("You have " + player.CurrentHitPoints + " HP left.");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    player.TakeDamage(monster.MaximumDamage);
+                    Console.WriteLine("The " + monster.Name + " hit you for " + monster.MaximumDamage + " damage.");
+                    Console.WriteLine("You have " + player.CurrentHitPoints + " HP left.");
+                    Console.WriteLine();
+                }
             }
 
             if (monster.CurrentHitPoints <= 0)
