a322bc3950cdc8c0efbbb969b520f02d489064d7
Stijn231106 <stijnvandermunnik1@gmail.com>
2026-03-10 14:25:35 +0100

    kleine push

--- ProjectAlpha/Program.cs
+++ ProjectAlpha/Program.cs
@@ -161,14 +161,9 @@ class Program
 
 
             player.Potions.Add(World.PotionByID(World.POTION_ID_HEAL));
-<<<<<<< Updated upstream
-            Console.WriteLine("\nReward: Heal Potion added to your inventory!");
-            Console.WriteLine("[NEW] Head to the Bridge for your next quest!\n");
-=======
             player.Potions.Add(World.WeaponByID(World.WEAPON_ID_FARMERS_PITCHFORK));
             Console.WriteLine("Reward: Heal Potion added to your inventory!");
             Console.WriteLine("[NEW] Head to the Bridge for your next quest!");
->>>>>>> Stashed changes
         }
     }
 
