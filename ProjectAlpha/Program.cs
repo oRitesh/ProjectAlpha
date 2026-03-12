7c3d017f32ac0fa3e63fd190af9744e16070f200
oRitesh <ramai@outlook.com>
2026-03-12 09:44:30 +0100

    kill multiple fix

--- ProjectAlpha/Program.cs
+++ ProjectAlpha/Program.cs
@@ -28,8 +28,15 @@ class Program
 
             CheckForQuest(player);
             CheckForHealing(player);
-            CheckForMonster(player);
+
+            bool battled = CheckForMonster(player);
             CheckForQuestCompletion(player);
+
+            if (battled)
+            {
+                continue;
+            }
+
             ShowAvailableDirections(player);
 
             Console.Write("Choose an option:\n- Enter direction (N/E/S/W)>\n- I for inventory>\n- C to change weapon>\n- Q to quit>\n-> ");
@@ -51,7 +58,6 @@ class Program
             MovePlayer(player, input);
         }
     }
-
     static void CheckForHealing(Player player)
     {
         if (player.CurrentLocation.ID == World.LOCATION_ID_HOME
@@ -91,14 +97,13 @@ class Program
         }
     }
 
-    static void CheckForMonster(Player player)
+    static bool CheckForMonster(Player player)
     {
-
         Monster monster = player.CurrentLocation.MonsterLivingHere;
 
         if (monster == null)
         {
-            return;
+            return false;
         }
 
         bool battleResult = BattleSystem.StartBattle(player, monster);
@@ -112,7 +117,7 @@ class Program
         if (!battleResult)
         {
             Console.WriteLine("You fled from the battle.");
-            return;
+            return false; // let player choose where to move next
         }
 
         if (monster.ID == World.MONSTER_ID_RAT) ratsKilled++;
@@ -133,16 +138,15 @@ class Program
 
         if (currentKills < 3)
         {
-            monster.CurrentHitPoints = monster.MaximumDamage; // Reset monster's HP for next battle
-            return;
+            monster.CurrentHitPoints = monster.MaximumHitPoints;
+            return true; // won battle, fight again next loop
         }
-
         else
         {
             Console.WriteLine("\nYou have defeated all the monsters in this location!");
             player.CurrentLocation.MonsterLivingHere = null;
+            return true;
         }
-
     }
     static void CheckForQuestCompletion(Player player)
     {
